6fd785804fac6b4754274b12374ad68b3599c2e1
Rabis Chawarri <rabischawarri@gmail.com>
2025-01-11 15:45:32 +0100

    completed 2.12

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,25 +1,29 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import Filter from './components/Filter';
 import PersonForm from './components/PersonForm';
 import Persons from './components/Persons';
+import axios from 'axios'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
 
   const [newName, setNewName] = useState('')
 
   const [newNumber, setNewNumber] = useState('')
 
-  const [newId, setNewId] = useState(5)
-
   const [newFilter, setNewFilter] = useState('')
 
   const [filterActive, setFilterActive] = useState(false)
+
+  const hook = () => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data)
+      })
+  }
+
+  useEffect(hook, [])
 
   const handleNameChange = (event) => {
     setNewName(event.target.value)
@@ -46,8 +50,7 @@ const App = () => {
     event.preventDefault()
     const personObject = {
       name: newName,
-      number: newNumber,
-      id: newId
+      number: newNumber
     }
     let duplicateEntry = false
     let duplicateNumber = false
@@ -67,10 +70,14 @@ const App = () => {
     }
 
     else {
-      setPersons(persons.concat(personObject))
-      setNewName('')
-      setNewNumber('')
-      setNewId(newId + 1)
+      axios
+        .post('http://localhost:3001/persons', personObject)
+        .then(response => {
+          setPersons(persons.concat(response.data))
+          setNewName('')
+          setNewNumber('')
+        })
+
     }
   }
 
