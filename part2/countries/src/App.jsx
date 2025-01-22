dce0cd808d181c4d89920163f04d950feec9ef31
Rybis02 <rabischawarri@gmail.com>
2025-01-22 19:28:18 +0100

    changed code to work with usestate(null)

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -1,51 +1,53 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react';
 import Search from './Components/Search';
-import Notification from './Components/Notification';
-import axios from 'axios'
+import Notification from './Components/Notification'; 
+import axios from 'axios';
 
 function App() {
   const [search, setSearch] = useState('')
-  const [allCountries, setAllCountries] = useState([])
-  const [filteredCountries, setFilteredCountries] = useState([])
+  const [allCountries, setAllCountries] = useState(null)
+  const [filteredCountries, setFilteredCountries] = useState(null)
 
-  const handleSearch = (event) => {
-    const newSearch = event.target.value
-    setSearch(newSearch)
-    filterCountries(allCountries, newSearch)
-  }
-
-  const filterCountries = (countries, searchValue) => {
-    setFilteredCountries((countries.filter(elem => 
-      elem.name.common.toLowerCase().includes(searchValue.toLowerCase())
-    )))
-  }
-
-  const final = search.length !== 0 ? filteredCountries : allCountries
-
-  const getAllCountries = () => {
-    if (allCountries) {
-      axios
-        .get(`https://studies.cs.helsinki.fi/restcountries/api/all`)
-        .then(response => {
-          setAllCountries(response.data)
-        })
-        .catch(error => {
-          console.log(error)
-        })
+  const getAllCountries = async () => {
+    try {
+      const response = await axios.get('https://studies.cs.helsinki.fi/restcountries/api/all')
+      setAllCountries(response.data)
+      setFilteredCountries(response.data)
+    } catch (error) {
+      console.error('Error fetching countries:', error)
     }
   }
 
-  useEffect(getAllCountries, [])
+  const handleSearch = (event) => {
+    const newSearch = event.target.value;
+    setSearch(newSearch)
+
+    if (allCountries) {
+      const lowercasedSearch = newSearch.toLowerCase()
+      const filtered = allCountries.filter((country) =>
+        country.name.common.toLowerCase().includes(lowercasedSearch)
+      )
+      setFilteredCountries(filtered.length > 0 ? filtered : null)
+    }
+  }
+
+  useEffect(() => {
+    getAllCountries()
+  }, [])
+
+  if (!allCountries) {
+    return <div>Loading countries...</div>
+  }
 
   return (
-    <>
-      <div>
-        <Search input={search} handler={handleSearch} />
-      </div>
-      <div>
-        <Notification countries={final} buttonHandler={setFilteredCountries} />
-      </div>
-    </>
+    <div>
+      <Search input={search} handler={handleSearch} />
+      {filteredCountries ? (
+        <Notification countries={filteredCountries} />
+      ) : (
+        <div>No countries found matching your search.</div>
+      )}
+    </div>
   )
 }
 
